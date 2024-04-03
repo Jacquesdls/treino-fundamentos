f4ce74774653d7b7f14b60faa551f9a2877bd577
Jacquesdls <jacques.wsa@gmail.com>
2024-04-02 21:41:14 -0300

    treino-fundamentos

---
+++ aula27.py
@@ -0,0 +1,12 @@
+variavel = 'Olá mundo'
+print(variavel[5])
+print(variavel[-4])
+print(variavel[4:])#exibi o resto dos caracteres apartir do indice determinado
+print(variavel[4:7])
+print(variavel[:4])
+print(len(variavel))#conta quantos caracteres tem a variavel
+print(variavel[0:len(variavel):1])
+print(variavel[0:3:1])
+print(variavel[0:len(variavel):2])
+print(variavel[::-1])
+print(variavel[-1:-10:-1])
---
+++ aula34D.py
@@ -0,0 +1,13 @@
+""" 
+    Repetições
+    while(enquanto)
+"""
+condicao = True
+while condicao:
+    print(1)
+    print(2)
+    print(3)
+    condicao = input("Tecle [ENTER] \
+        para terminar ou 1 para continuar!")
+    
+#como while sabe que o "1" é True?
